570737feda988e61321c7a333ac757ce5517a192
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-01-26 21:27:08 +0800

    Add api docs

--- src/main/java/shortbus/Request.java
+++ src/main/java/shortbus/Request.java
@@ -1,5 +1,10 @@
 package shortbus;
 
+/**
+ * Marker interface to represent a request.
+ *
+ * @param <T> Expected result type
+ */
 public interface Request<T> {
 
 }
--- src/main/java/shortbus/Response.java
+++ src/main/java/shortbus/Response.java
@@ -1,9 +1,24 @@
 package shortbus;
 
+/**
+ * Response object given from Request.
+ *
+ * @param <T> Result type
+ */
 public class Response<T> {
+	/**
+	 * Request's result data
+	 */
 	public T data;
+	/**
+	 * Request's result exception (if there's any)
+	 */
 	public Exception exception;
 
+	/**
+	 * Return if request has exception
+	 * @return true if exception occured; otherwise false
+	 */
 	public boolean hasException() {
 		return exception != null;
 	}
